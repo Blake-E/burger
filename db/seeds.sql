6f337494d091d88a8bd19a46430f40b0067bf480
Blake-E <mbe083@gmail.com>
2019-01-24 21:08:08 -0600

    pushing to repo

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name) VALUES ('Vegan');
+INSERT INTO burgers (name) VALUES ('Jucy Lucy');
+INSERT INTO burgers (name) VALUES ('Quadruple Bypass');
+INSERT INTO burgers (name) VALUES ('BBQ');
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name) VALUES ('W.C. Slider');
+INSERT INTO burgers (name) VALUES ('Ramen')
