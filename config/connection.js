556c7b78906c1fddecb124002726377922366f7e
Blake-E <mbe083@gmail.com>
2019-01-29 20:49:56 -0600

    updated burgers homework

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
+// Modeled after the CatsApp from class activity //
 var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
+} else {
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -12,7 +13,7 @@ if (process.env.JAWSDB_URL) {
     database: "burgers_db"
   });
 }
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,45 +1,38 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-    $(".change-devoured").on("click", function(event) {
-      var id = $(this).data("id");
-     // var newDevoured = $(this).data("newDevoured");
-      var newDevoured = $(this).attr("data-newDevoured");
-      var newState = {
-        devoured: newDevoured
-      };
-  console.log(newDevoured);
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newState
-      }).then(
-        function() {
-          console.log("changed devoured to", newDevoured);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var newBurger = {
-        name: $("#bu").val().trim(),
-        devoured: $("[name=devoured]:checked").val().trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+// Modeled after the CatsApp from class activity //
+$(function () {
+  $(".change-devoured").on("click", function (event) {
+    var id = $(this).data("id");
+    var newDevoured = $(this).attr("data-newDevoured");
+    var newState = {
+      devoured: newDevoured
+    };
+    console.log(newDevoured);
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newState
+    }).then(
+      function () {
+        console.log("changed devoured to", newDevoured);
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function (event) {
+    event.preventDefault();
+    var newBurger = {
+      name: $("#bu").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function () {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
 });
